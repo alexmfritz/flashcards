955a2a66fd2956d805adda1cab0a3ca8aeaa759d
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-29 15:48:14 -0800

    Create describe and first it block for instanceOf Turn

--- test/Turns-test.js
+++ test/Turns-test.js
@@ -3,3 +3,12 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/card');
 
+describe('Turn', () => {
+
+  it('should be a function', () => {
+    const turn = Turn();
+    expect(turn).to.be.an.instanceOf(Turn);
+  });
+
+
+});
