a15282bcde6e9657b6b10866d2ca638fbbdee946
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-30 15:09:34 -0800

    Create it block for Round testing for adding incorrectGuesses

--- test/Round-test.js
+++ test/Round-test.js
@@ -82,6 +82,19 @@ describe('Round', () => {
     expect(round.turns).to.deep.equal(2);
   });
 
-  it('should be able to ')
+  it('should be able to add incorrect guesses', () => {
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+
+    const deck = new Deck([card3, card5, card22]);
+
+    const round = new Round(deck);
+
+    round.takeTurn('accessor method');
+    round.takeTurn('iteration method');
+
+    expect(round.incorrectGuesses.length).to.deep.equal(2);
+  });
 
 });
