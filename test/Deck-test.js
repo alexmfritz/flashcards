aef82b809f9060f67703d85b3cb29626c66a2b0c
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-29 20:47:46 -0800

    Create it block for Deck testing how many cards are in the deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -34,7 +34,16 @@ describe('Deck', () => {
         answers: ["Object.keys()", "Object.values()", "reduce()"],
         correctAnswer: "Object.keys()"
       }]);
-
-
   });
-})
+
+  it('should be able to count how many cards are in the deck', () => {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ["object", "array", "function"], 'object');
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+
+    const deck = new Deck([deck1, deck5, deck22]);
+
+    expect(deck.countCards()).to.equal(3);
+  });
+
+});
