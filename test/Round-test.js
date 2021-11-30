143f07e3c8f1a0fd6bb4960b5c3ddcc2ce46634e
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-30 15:48:52 -0800

    Create it block for Round testing for percent correct guesses

--- test/Round-test.js
+++ test/Round-test.js
@@ -138,4 +138,24 @@ describe('Round', () => {
     expect(wrongAnswer).to.deep.equal('incorrect!');
   });
 
+  it('should be able to calculate the percentage of correct answers', () => {
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+    const card29 = new Card(29, 'map() takes in two optional arguments: the index of the current element, and the array that map was called upon', ["true", "false"], 'true');
+
+    const deck = new Deck([card3, card5, card22, card29]);
+
+    const round = new Round(deck);
+
+    round.takeTurn('mutator method');
+    round.takeTurn('accessor method');
+    round.takeTurn('Object.values()');
+    round.takeTurn('true');
+
+    expect(round.incorrectGuesses).to.deep.equal([5, 9]);
+    expect(round.turns).to.deep.equal(4);
+    expect(round.calculatePercentageCorrect()).to.deep.equal(50)
+  });
+
 });
