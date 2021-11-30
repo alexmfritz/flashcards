b66bdfe074acaaba9337a82d76897f5f41d67397
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-30 14:25:41 -0800

    Create it block for Round testing returnCurrentCard method

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
-const Turn = require('../src/Turns');
+const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe('Round', () => {
@@ -26,7 +26,21 @@ describe('Round', () => {
 
     const round = new Round(deck);
 
-    expect(round.deck.length).to.equal(1);
+    expect(round.deck).to.equal([card3, card5, card22]);
+  });
+
+  it('should be able to return the current card', () => {
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+
+    const deck = new Deck([card3, card5, card22]);
+
+    const round = new Round(deck);
+
+    const returnedCard = round.returnCurrentCard();
+
+    expect(returnedCard).to.equal(round.currentCard);
   });
 
 });
