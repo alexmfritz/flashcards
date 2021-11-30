358d62a3ffc14e49852c7d7b477a727445517083
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-30 14:27:59 -0800

    Create it block for Round testing default turns property value

--- test/Round-test.js
+++ test/Round-test.js
@@ -43,4 +43,16 @@ describe('Round', () => {
     expect(returnedCard).to.equal(round.currentCard);
   });
 
+  it('should keep track of turns, starting at 0', () => {
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+
+    const deck = new Deck([card3, card5, card22]);
+
+    const round = new Round(deck);
+
+    expect(round.turns).to.equal(0);
+  });
+
 });
