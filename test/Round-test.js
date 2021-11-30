f428b8f1cec98d70dbd7835e9b7b7fb84e72a9f7
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-30 15:16:28 -0800

    Create it block for Round testing showing next card

--- test/Round-test.js
+++ test/Round-test.js
@@ -38,9 +38,8 @@ describe('Round', () => {
 
     const round = new Round(deck);
 
-    const returnedCard = round.returnCurrentCard();
 
-    expect(returnedCard).to.equal(round.currentCard);
+    expect(round.returnCurrentCard()).to.deep.equal(card3);
   });
 
   it('should keep track of turns, starting at 0', () => {
@@ -82,6 +81,20 @@ describe('Round', () => {
     expect(round.turns).to.deep.equal(2);
   });
 
+  it('should be able to show the next card', () => {
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+
+    const deck = new Deck([card3, card5, card22]);
+
+    const round = new Round(deck);
+
+    round.takeTurn('mutator method');
+
+    expect(round.currentCard).to.deep.equal(card5);
+  });
+
   it('should be able to add incorrect guesses', () => {
     const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
     const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
@@ -92,9 +105,11 @@ describe('Round', () => {
     const round = new Round(deck);
 
     round.takeTurn('accessor method');
-    round.takeTurn('iteration method');
+    round.takeTurn('mutator method');
 
     expect(round.incorrectGuesses.length).to.deep.equal(2);
   });
+
+
 
 });
