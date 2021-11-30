389c8295cc4433e355aad84908656131832d1483
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-30 15:22:59 -0800

    Create it block for Round testing for incorrect answer

--- test/Round-test.js
+++ test/Round-test.js
@@ -124,6 +124,18 @@ describe('Round', () => {
     expect(correctAnswer).to.deep.equal('correct!');
   });
 
-  
+  it('should be able to give feedback if incorrect', () => {
+    const card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
+    const card5 = new Card(5, 'What type of prototype method loops through the existing array and applies a callback function that may mutate each element and return a new value?', ["mutator method", "accessor method", "iteration method"], 'iteration method');
+    const card22 = new Card(22, 'Which prototype method is used to iterate over each property of an object?', ["Object.keys()", "Object.values()", "reduce()"], 'Object.keys()');
+
+    const deck = new Deck([card3, card5, card22]);
+
+    const round = new Round(deck);
+
+    const wrongAnswer = round.takeTurn('iteration method');
+
+    expect(wrongAnswer).to.deep.equal('incorrect!');
+  });
 
 });
