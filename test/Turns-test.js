c9a737742e99604b239726bae263f2eef26f1fb3
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-01 14:20:46 -0800

    Refactor Turn-test to reflect new data-test.js file

--- test/Turns-test.js
+++ test/Turns-test.js
@@ -9,8 +9,8 @@ describe('Turn', () => {
 
   beforeEach( () => {
     
-    card = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
-    turn = new Turn('mutator method', card);
+    card = new Card(1, 'What are Alex\'s favorite colors?', ['red and yellow', 'blue and green', 'purple and pink'], 'blue and green');
+    turn = new Turn('blue and green', card);
   });
 
   it('should be a function', () => {
@@ -20,7 +20,7 @@ describe('Turn', () => {
 
   it('should be able to store a player\'s guess', () => {
 
-    expect(turn.guess).to.equal('mutator method');
+    expect(turn.guess).to.equal('blue and green');
   });
 
   it('should be able to store the current card', () => {
@@ -31,7 +31,7 @@ describe('Turn', () => {
   it('should be able to return the player\'s guess', () => {
     const returnedGuess = turn.returnGuess();
 
-    expect(returnedGuess).to.equal('mutator method');
+    expect(returnedGuess).to.equal('blue and green');
   });
 
   it('should be able to return the card', () => {
@@ -41,8 +41,8 @@ describe('Turn', () => {
   });
 
   it('should be able to evaluate the player\'s guess against the correct answer', () => {
-    const wrongTurn = new Turn('iteration method', card);
-    const rightTurn = new Turn('mutator method', card);
+    const wrongTurn = new Turn('red and yellow', card);
+    const rightTurn = new Turn('blue and green', card);
 
     const wrongAnswer = wrongTurn.evaluateGuess();
     const rightAnswer = rightTurn.evaluateGuess();
@@ -52,8 +52,8 @@ describe('Turn', () => {
   });
 
   it('should be able to give feedback to the player based on their answer', () => {
-    const wrongTurn = new Turn('iteration method', card);
-    const rightTurn = new Turn('mutator method', card);
+    const wrongTurn = new Turn('red and yellow', card);
+    const rightTurn = new Turn('blue and green', card);
 
     const wrongAnswer = wrongTurn.giveFeedback();
     const rightAnswer = rightTurn.giveFeedback();
