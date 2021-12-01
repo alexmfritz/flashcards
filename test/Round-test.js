960d9585d48e8ba886bd5f14e7495421591954bd
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-01 14:21:04 -0800

    Refactor Round-test to reflect new data-test.js file

--- test/Round-test.js
+++ test/Round-test.js
@@ -3,17 +3,17 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
-const Turn = require('../src/Turns');
-const data = require('../src/data');
+const data = require('../src/test-data');
 
 describe('Round', () => {
   let deck;
   let round;
-  let cards = [];
-  data.prototypeData.forEach(card => cards.push(new Card(card.id, card.question, card.answers, card.correctAnswer)));
+  let cards;
 
   beforeEach( () => {
-
+    cards = data.testData.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    });
     deck = new Deck(cards);
     round = new Round(deck);
   });
@@ -30,7 +30,7 @@ describe('Round', () => {
 
   it('should be able to hold a deck of cards', () => {
 
-    expect(round.deck.cards.length).to.equal(30);
+    expect(round.deck.cards.length).to.equal(4);
   });
 
   it('should be able to return the current card', () => {
@@ -52,55 +52,55 @@ describe('Round', () => {
   });
 
   it('should be able to take a turn, updating the turn count', () => {
-    round.takeTurn('array');
-    round.takeTurn('function');
+    round.takeTurn('blue and green');
+    round.takeTurn('polar bear');
 
     expect(round.turns).to.deep.equal(2);
   });
 
   it('should be able to show the next card', () => {
-    round.takeTurn('mutator method');
+    round.takeTurn('red and yellow');
     const returnedCard = round.returnCurrentCard();
 
     expect(returnedCard).to.deep.equal(round.currentCard);
   });
 
   it('should be able to evaluate incorrect guesses and store them', () => {
-    round.takeTurn('array');
-    round.takeTurn('object');
+    round.takeTurn('red and yellow');
+    round.takeTurn('purple and pink');
 
     expect(round.incorrectGuesses.length).to.deep.equal(2);
   });
 
   it('should be able to give feedback if correct', () => {
-    const correctAnswer = round.takeTurn('object');
+    const correctAnswer = round.takeTurn('blue and green');
 
     expect(correctAnswer).to.deep.equal('correct!');
   });
 
   it('should be able to give feedback if incorrect', () => {
-    const wrongAnswer = round.takeTurn('function');
+    const wrongAnswer = round.takeTurn('red and yellow');
 
     expect(wrongAnswer).to.deep.equal('incorrect!');
   });
 
   it('should be able to calculate the percentage of correct answers', () => {
-    round.takeTurn('object');
-    round.takeTurn('function');
-    round.takeTurn('iteration method');
-    round.takeTurn('accessor method');
+    round.takeTurn('blue and green');
+    round.takeTurn('octopus');
+    round.takeTurn('April');
+    round.takeTurn('Lauren');
 
-    expect(round.incorrectGuesses).to.deep.equal([2, 3]);
+    expect(round.incorrectGuesses).to.deep.equal([3]);
     expect(round.turns).to.deep.equal(4);
-    expect(round.calculatePercentageCorrect()).to.deep.equal(50)
+    expect(round.calculatePercentageCorrect()).to.equal(75)
   });
 
   it('should notify the player when the round is over', () => {
-    round.takeTurn('object');
-    round.takeTurn('array');
-    round.takeTurn('mutator method');
+    round.takeTurn('blue and green');
+    round.takeTurn('octopus');
+    round.takeTurn('April');
+    round.takeTurn('Lauren');
 
-    expect(round.turns).to.deep.equal(3);
-    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 100% of the questions correctly');
+    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 75% of the questions correctly');
   });
 });
