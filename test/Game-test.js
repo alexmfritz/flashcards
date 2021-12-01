95cba0ba90a2e9160073143c7a2bf0e6d9bf89d0
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-01 09:45:35 -0800

    Refactor Game class test-suit for errors

--- test/Game-test.js
+++ test/Game-test.js
@@ -14,7 +14,7 @@ describe('Game', () => {
 
   it('should be a function', () => {
 
-    expect(Game).to.be('function');
+    expect(Game).to.be.a('function');
   });
 
   it('should be an instance of Game', () => {
@@ -24,13 +24,19 @@ describe('Game', () => {
 
   it('should be able to keep track of the current round', () => {
 
-    expect(game.currentRound).to.exist;
+    expect(game).to.have.a.property('currentRound');
   });
 
   it('should be able to create new cards', () => {
     game.start();
 
-    expect(game.deck.cards).to.exist;
+    expect(game).to.have.a.property('deck');
+  });
+
+  it('should be able to create a new round', () => {
+    game.start();
+
+    expect(game).to.have.a.property('round');
   });
 
   it('should be able to put cards in the deck', () => {
