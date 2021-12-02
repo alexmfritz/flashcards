f209789476695f2f053c91febd3cd1a64db20132
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-01 23:29:34 -0800

    Create a new instance of Turn on a Round class property instead of local variable

--- src/Round.js
+++ src/Round.js
@@ -13,8 +13,8 @@ class Round {
   }
 
   takeTurn(guess) {
-    let turn = new Turn(guess, this.currentCard);
-    if (!turn.evaluateGuess()) {
+    this.currentTurn = new Turn(guess, this.currentCard);
+    if (!this.currentTurn.evaluateGuess()) {
       let id = this.currentCard.id
       this.incorrectGuesses.push(id);
     }
@@ -22,7 +22,7 @@ class Round {
     this.turns++;
     this.currentCard = this.deck.cards[this.turns];
 
-    return turn.giveFeedback();
+    return this.currentTurn.giveFeedback();
   }
 
   calculatePercentageCorrect() {
